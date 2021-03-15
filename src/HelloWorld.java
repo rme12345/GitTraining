a74bb04439aa47bde5be78338232b63d8994ba8b
Rebecca Ewart <rebecca.ewart@acc.co.nz>
2021-03-16 11:13:40 +1300

    Initial Commit Master

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,7 @@
 public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
+        System.out.println("This is my first commit");
+        System.out.println("This is my second commit");
     }
 }
