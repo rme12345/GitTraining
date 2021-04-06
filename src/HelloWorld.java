22e3b4e97869b421bf2fc2a9a464aae58f407aed
Rebecca Ewart <rebecca.ewart@acc.co.nz>
2021-04-07 11:32:13 +1200

    this better work

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -2,6 +2,6 @@ public class HelloWorld {
     public static void main(String[] args){
         System.out.println("Hello World");
         System.out.println("This is my first commit");
-        System.out.println("This is my second commit");
+        System.out.println("This is my best commit");
     }
 }
