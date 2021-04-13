65f2158cbe845382200cc652b5f8a5cc914eba85
Rebecca Ewart <rebecca.ewart@acc.co.nz>
2021-04-13 15:00:06 +1200

    reversal of numbers

--- src/Reversed.java
+++ src/Reversed.java
@@ -2,8 +2,15 @@ import java.util.Scanner;
 
 public class Reversed {
     public static void main(String[] args) {
+        int n, reverse = 0;
         Scanner sc = new Scanner(System.in); //Creating the object for scanner class and passing the input stream from the key board
         System.out.println("Please Enter a whole number");
-        int subject1 = sc.nextInt();
+        n = sc.nextInt();
+                while (n !=0 ){
+                reverse = reverse *10;
+    reverse = reverse + n%10;
+    n=n/10;
+            }
+        System.out.println("Reverse of the number is " + reverse);
     }
 }
