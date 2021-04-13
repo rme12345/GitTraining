7f28b94acd9de8dd60213d1f7e397cdfa8e3bfe8
Rebecca Ewart <rebecca.ewart@acc.co.nz>
2021-04-13 14:45:01 +1200

    I hope this works

---
+++ src/Reversed.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Reversed {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in); //Creating the object for scanner class and passing the input stream from the key board
+        System.out.println("Please Enter a whole number");
+        int subject1 = sc.nextInt();
+    }
+}
--- src/Tuna.java
+++
@@ -1,8 +0,0 @@
-public class Tuna {
-   public void simpleMessage(String name){
-       System.out.println("Hello " + name);
-       System.out.println("Does this work " + name);
-   }
-
-
-}
