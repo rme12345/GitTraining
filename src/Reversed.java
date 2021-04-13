9cf8975282d998c1819c4f0aa9476f8d7a624219
Rebecca Ewart <rebecca.ewart@acc.co.nz>
2021-04-14 07:19:50 +1200

    this is another commit

--- src/Reversed.java
+++ src/Reversed.java
@@ -4,12 +4,12 @@ public class Reversed {
     public static void main(String[] args) {
         int n, reverse = 0;
         Scanner sc = new Scanner(System.in); //Creating the object for scanner class and passing the input stream from the key board
-        System.out.println("Please Enter a whole number");
+        System.out.println("Please enter a whole number");
         n = sc.nextInt();
                 while (n !=0 ){
                 reverse = reverse *10;
-    reverse = reverse + n%10;
-    n=n/10;
+                reverse = reverse + n%10;
+            n=n/10;
             }
         System.out.println("Reverse of the number is " + reverse);
     }
