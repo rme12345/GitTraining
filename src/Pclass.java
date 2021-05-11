58592a7b1ec75d149a1d541da37a939725a7a29b
Rebecca Ewart <rebecca.ewart@acc.co.nz>
2021-05-11 14:13:53 +1200

    this is a new assignment

---
+++ src/Pclass.java
@@ -0,0 +1,21 @@
+class Pclass {
+    public void pmethod() {
+        System.out.println("This is the parent class");
+    }
+}
+class Cclass extends Pclass{
+    public void cmethod(){
+        System.out.println("This is the child class");
+
+    }
+   }
+class Ans{
+    public static void main(String[] args){
+        Pclass m = new Pclass();
+        Cclass n = new Cclass();
+        m.pmethod();
+        n.cmethod();
+        n.pmethod();
+    }
+
+}
